5d28dd038caee337f38246b21e6a4df9822cb2e9
Rafael José <rafael_013@hotmail.com>
2024-09-11 07:31:50 -0300

    criate menu

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const start = () => {
+const {select} = require("@inquirer/prompts")
+
+const start = async() => {
     while(true){
-        let opção = "sair"
+        
+        const opção = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        
         switch(opção){
             case "cadastrar":
-                console.log("Cadastre uma atividade")
+                console.log("Cadastre uma meta")
                 break
             case "listar":
-                console.log("Liste uma atividade")
+                console.log("Liste uma meta")
                 break
             case "sair":
+                console.log("Até a próxima")
                 return
-                    }
+         }
     }
 }
 
