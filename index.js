e302d23896e32ff78d3324532fba3b81d7fc8009
Rafael José <rafael_013@hotmail.com>
2024-09-10 22:50:57 -0300

    Alt index 01

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-//Hello World
-console.log("Hello World")
+let meta = {
+    value: "Ler um livro por mês",
+    checked: true
+}
+
+let metas = [
+    meta,
+    {
+        value: "Correr 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
